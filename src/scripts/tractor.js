2ce4e65227e908eb7ddd372e245474dbefefe246
fimoefive <brothermj@hotmail.com>
2020-09-23 07:27:22 -0500

    tractor.js and main.js edits.

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -4,9 +4,19 @@ import { createPotato } from "./seeds/potato.js";
 import { createSoyBean } from "./seeds/soybean.js";
 import { createSunFlower } from "./seeds/sunflower.js";
 import { createWheat } from "./seeds/wheat.js";
+import {addPlant, usePlants} from "./field.js";
 
 
 export const plantSeeds = (plantArray) => {
+for(cosnt plantRows of planArray) {
+    for(cosnt row of planRows) {
+
+
+
+        
+    }
+}
+
     [
         ["Potato", "Soybean", "Soybean", "Corn"],
         ["Wheat", "Corn", "Wheat", "Asparagus"],
